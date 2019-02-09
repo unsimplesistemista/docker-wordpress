9fd5b5373d746d3ffd80fc5078889120b3988634
Manel Martinez <manelmartinez@MAC-0215-60.local>
2019-02-09 08:46:30 +0100

    Create Install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOCKER_COMPOSE_VERSION=1.23.2
-DOCKER_COMPOSE_FILE=https://github.com/unsimplesistemista/docker-wordpress/blob/master/docker-compose.yml
+DOCKER_COMPOSE_FILE=https://raw.githubusercontent.com/unsimplesistemista/docker-wordpress/master/docker-compose.yml
 DOCKER_COMPOSE_FILE_TMP=/tmp/1ss-docker-compose.yml
 DOCKER_COMPOSE_PROJECT=wordpress
 
