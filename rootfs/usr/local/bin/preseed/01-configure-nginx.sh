80b70feeb8294bdde854896d5768e883e16289e9
Manel Martinez <manelmartinez@MAC-0215-60.local>
2019-10-07 13:01:30 +0200

    Fix

--- rootfs/usr/local/bin/preseed/01-configure-nginx.sh
+++ rootfs/usr/local/bin/preseed/01-configure-nginx.sh
@@ -5,5 +5,5 @@ if [ ${CACHE_ENABLED} -eq 1 ]; then
 else
   SKIP_CACHE=1
 fi
-perl -p -i -e "s/#SKIP_CACHE#/${SKIP_CACHE}/g" /etc/nginx/sites-enabled/*
-perl -p -i -e "s/#CACHE_200_MINUTES#/${CACHE_200_MINUTES}/g" /etc/nginx/sites-enabled/*
+perl -p -i -e "s/#SKIP_CACHE#/${SKIP_CACHE}/g" /etc/nginx/sites-available/*
+perl -p -i -e "s/#CACHE_200_MINUTES#/${CACHE_200_MINUTES}/g" /etc/nginx/sites-available/*
