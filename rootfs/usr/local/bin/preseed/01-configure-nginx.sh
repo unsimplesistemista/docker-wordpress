7d1b94dfe18ac3159a80f961f2749f2527b629e7
Manel Martinez <manelmartinez@MAC-0215-60.local>
2019-10-10 20:39:19 +0200

    Add support for subfolder wordpress installations

--- rootfs/usr/local/bin/preseed/01-configure-nginx.sh
+++ rootfs/usr/local/bin/preseed/01-configure-nginx.sh
@@ -10,9 +10,9 @@ perl -p -i -e "s/#CACHE_200_MINUTES#/${CACHE_200_MINUTES}/g" /etc/nginx/sites-av
 
 # Make it possible to run wordpress sites in subfolders ...
 for wordpress_subfolder in ${WORDPRESS_SUBFOLDERS}; do
-  wordpress_subfolder=`echo ${wordpress_subfolder} | sed "s/\/$//g"`
+  wordpress_subfolder=`echo ${wordpress_subfolder} | sed "s/\/$//g" | sed "s/$/\//g"`
   wordpress_subfolder_scaped=`echo ${wordpress_subfolder} | sed "s/\//\\\\\\\\\//g"`
   echo "=> Enabling Wordpress subfolder ${wordpress_subfolder} ..."
-  perl -p -i -e "s/#WORDPRESS_SUBFOLDERS_HERE#/#WORDPRESS_SUBFOLDERS_HERE#\n\n    location ${wordpress_subfolder_scaped} {\n        try_files \\\$uri \\\$uri\/ ${wordpress_subfolder_scaped}\/index.php\\\$is_args\\\$args;\n    }/g" /etc/nginx/sites-available/*
+  perl -p -i -e "s/#WORDPRESS_SUBFOLDERS_HERE#/#WORDPRESS_SUBFOLDERS_HERE#\n\n    location ${wordpress_subfolder_scaped} {\n        try_files \\\$uri \\\$uri\/ ${wordpress_subfolder_scaped}index.php\\\$is_args\\\$args;\n    }/g" /etc/nginx/sites-available/*
 done
 perl -p -i -e "s/#WORDPRESS_SUBFOLDERS_HERE#//g" /etc/nginx/sites-available/*
