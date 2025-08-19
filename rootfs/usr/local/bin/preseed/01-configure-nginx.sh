dbfb3b49e5f956e14e8a3ab239b6dac85bccdd6e
Manel <manel@nixelsolutions.com>
2025-08-19 13:06:37 +0200

    Parametrize RATE_LIMIT

--- rootfs/usr/local/bin/preseed/01-configure-nginx.sh
+++ rootfs/usr/local/bin/preseed/01-configure-nginx.sh
@@ -10,7 +10,7 @@ RATE_LIMIT_SCAPED=`echo ${RATE_LIMIT} | sed "s/\//\\\\\\\\\//g"`
 
 perl -p -i -e "s/#SKIP_CACHE#/${SKIP_CACHE}/g" /etc/nginx/sites-available/*
 perl -p -i -e "s/#CACHE_200_MINUTES#/${CACHE_200_MINUTES}/g" /etc/nginx/sites-available/*
-perl -p -i -e "s/#RATE_LIMIT#/${RATE_LIMIT_SCAPED}/g" /etc/nginx/sites-available/*
+perl -p -i -e "s/#RATE_LIMIT#/${RATE_LIMIT_SCAPED}/g" /etc/nginx/conf.d/*
 if [ a"${SKIP_RATELIMIT_TOKEN}" != "a" ]; then
   perl -p -i -e "s/#SKIP_RATELIMIT_TOKEN#/\"~^0:${SKIP_RATELIMIT_TOKEN}\\$\"  \"\"; # You have the magic skip token/g" /etc/nginx/conf.d/zz-ddos.conf
 fi
